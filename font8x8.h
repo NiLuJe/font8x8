0878c5c7795bcef54826c646ed0fc043419471db
NiLuJe <ninuje@gmail.com>
2018-06-14 16:56:46 +0200

    Make everything static const

--- font8x8.h
+++ font8x8.h
@@ -1,9 +1,9 @@
-/** 
+/**
  * 8x8 monochrome bitmap fonts for rendering
  * Author: Daniel Hepper <daniel@hepper.net>
- * 
+ *
  * License: Public Domain
- * 
+ *
  * Based on:
  * http://dimensionalrift.homelinux.net/combuster/mos3/?p=viewsource&file=/modules/gfx/font8_8.asm
  **/
